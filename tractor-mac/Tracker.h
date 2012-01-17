4e4b335a4b2e7241fa0572b8d9fc794d0b7e145c
Michael Bass <mbass@macadamian.com>
2012-01-16 16:44:41 -0800

    some odd tests

---
+++ tractor-mac/Tracker.h
@@ -0,0 +1,13 @@
+//
+//  Tracker.h
+//  tractor-mac
+//
+//  Created by Michael Bass on 1/16/12.
+//  Copyright 2012 Macadamian. All rights reserved.
+//
+
+#import "Box2DSprite.h"
+
+@interface Tracker : Box2DSprite
+
+@end
---
+++ tractor-mac/Tracker.mm
@@ -0,0 +1,23 @@
+//
+//  Tracker.m
+//  tractor-mac
+//
+//  Created by Michael Bass on 1/16/12.
+//  Copyright 2012 Macadamian. All rights reserved.
+//
+
+#import "Tracker.h"
+
+@implementation Tracker
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
